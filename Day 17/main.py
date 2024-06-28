4cac4ef9c2fec0e6d5cd13e56d9b9291af6bf307
chickenONsteak <austinchenwd@gmail.com>
2024-06-28 16:45:27 +0800

    testing

--- Day 17/main.py
+++ Day 17/main.py
@@ -2,6 +2,7 @@ from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
 
+#Test comment
 question_bank = []
 for i in question_data:
     question = i["question"]
